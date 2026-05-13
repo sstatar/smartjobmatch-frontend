1a793dfc7bd2d10e066fa43441aa97f868da6252
sstatar <guitar0628393550@gmail.com>
2026-05-13 15:33:15 +0700

    chore: remove unused page and update api config

--- lib/api-config.ts
+++ lib/api-config.ts
@@ -1,6 +1,6 @@
 // lib/api-config.ts
 export const API_BASE_URL =
     process.env.NEXT_PUBLIC_API_BASE_URL ||
-    "http://192.168.1.105:3300/api/v1";
+    "http://localhost:3300/api/v1";
 export const RESUME_BASE_URL =
     process.env.NEXT_PUBLIC_RESUME_BASE_URL || "http://192.168.1.105:3300/uploads";
